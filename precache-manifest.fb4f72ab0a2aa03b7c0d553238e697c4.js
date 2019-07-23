68a7828f910a28e4f613f0f47f45bfeb600108db
Kenneth Buan <buancneth@gmail.com>
2019-07-24 05:33:01 +0800

    test manual react upload

---
+++ precache-manifest.fb4f72ab0a2aa03b7c0d553238e697c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9857ba922b7aec189ccf10abda2d75",
+    "url": "/Fortpanion/index.html"
+  },
+  {
+    "revision": "5607e489d86c57abceb1",
+    "url": "/Fortpanion/static/css/main.a6081261.chunk.css"
+  },
+  {
+    "revision": "413c0ba7da186dbdd944",
+    "url": "/Fortpanion/static/js/2.d44a61f9.chunk.js"
+  },
+  {
+    "revision": "5607e489d86c57abceb1",
+    "url": "/Fortpanion/static/js/main.db85f77d.chunk.js"
+  },
+  {
+    "revision": "fd636b1148c4f0af2bfa",
+    "url": "/Fortpanion/static/js/runtime~main.7177db7b.js"
+  }
+]);
